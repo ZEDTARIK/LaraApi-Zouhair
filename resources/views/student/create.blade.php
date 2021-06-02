4cbe14d1e1b611f0bdae42b370cac0a36437d885
ettarak <ettarak.zouhair@gmail.com>
2021-06-02 21:03:12 +0200

    Create one Form for Insert and Update and include them for both

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -12,56 +12,7 @@
 
             @csrf
 
-            <div class="form-group">
-                <label for="studentCode" class="col-sm-2 control-label">Student Code</label>
-                <div class="col-sm-4">
-                    <input type="text" class="form-control" name="studentCode" id="studentCode"
-                        placeholder="Student Code" value="{{ old('studentCode')}}">
-                </div>
-                @if($errors->any())
-                <div class="col-sm-4 has-error">
-                    @foreach($errors->get('studentCode') as $error)
-                    <span class="help-block">
-                        {{ $error }}
-                    </span>
-                    @endforeach
-                </div>
-                @endif
-            </div>
-
-            <div class="form-group">
-                <label for="firstName" class="col-sm-2 control-label">First Name</label>
-                <div class="col-sm-4">
-                    <input type="text" class="form-control" name="firstName" id="firstName" placeholder="First Name"
-                        value="{{ old('firstName')}}">
-                </div>
-                @if($errors->any())
-                <div class="col-sm-4 has-error">
-                    @foreach($errors->get('firstName') as $error)
-                    <span class="help-block">
-                        {{ $error }}
-                    </span>
-                    @endforeach
-                </div>
-                @endif
-            </div>
-
-            <div class="form-group">
-                <label for="lastName" class="col-sm-2 control-label">Last Name</label>
-                <div class="col-sm-4">
-                    <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Last Name"
-                        value="{{ old('lastName')}}">
-                </div>
-                @if($errors->any())
-                <div class="col-sm-4 has-error">
-                    @foreach($errors->get('lastName') as $error)
-                    <span class="help-block">
-                        {{ $error }}
-                    </span>
-                    @endforeach
-                </div>
-                @endif
-            </div>
+            @include('student.form')
 
             <div class="form-group">
                 <div class="col-sm-4">
---
+++ resources/views/student/form.blade.php
@@ -0,0 +1,52 @@
+<div class="form-group">
+    <label for="studentCode" class="col-sm-2 control-label">Student Code</label>
+    <div class="col-sm-4">
+        <input type="text" class="form-control" name="studentCode" id="studentCode" placeholder="Student Code"
+            value="{{ old('studentCode', $student->studentCode ?? null) }}" >
+    </div>
+    @if($errors->any())
+    <div class="col-sm-4 has-error">
+        @foreach($errors->get('studentCode') as $error)
+        <span class="help-block">
+            {{ $error }}
+        </span>
+        @endforeach
+    </div>
+    @endif
+</div>
+
+<div class="form-group">
+    <label for="firstName" class="col-sm-2 control-label">First Name</label>
+    <div class="col-sm-4">
+        <input type="text" class="form-control" name="firstName" id="firstName" placeholder="First Name"
+            value="{{ old('firstName', $student->firstName ??  null )}}">
+    </div>
+    @if($errors->any())
+    <div class="col-sm-4 has-error">
+        @foreach($errors->get('firstName') as $error)
+        <span class="help-block">
+            {{ $error }}
+        </span>
+        @endforeach
+
+    </div>
+    @endif
+</div>
+
+<div class="form-group">
+    <label for="lastName" class="col-sm-2 control-label">Last Name</label>
+    <div class="col-sm-4">
+        <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Last Name"
+            value="{{ old('lastName', $student->lastName ??  null )}}">
+    </div>
+    @if($errors->any())
+    <div class="col-sm-4 has-error">
+
+        @foreach($errors->get('lastName') as $error)
+        <span class="help-block">
+            {{ $error }}
+        </span>
+        @endforeach
+    </div>
+    @endif
+</div>
